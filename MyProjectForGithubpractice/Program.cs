4bc9b483bc841edc29402c2d835c1b500d5f4870
willgpa <wgpadgett@gmail.com>
2024-04-02 11:51:48 -0400

    Add project files.

---
+++ MyProjectForGithubpractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectForGithubpractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am sentient");
+        }
+    }
+}
